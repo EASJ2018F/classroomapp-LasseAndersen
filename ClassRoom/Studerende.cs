920cb3f0b546f38017c5bb92124d46d14d403879
Lasse Andersen <LasseAndersen@users.noreply.github.com>
2018-01-29 17:27:18 +0100

    opgave 1
    Tilføj en klasse (public) “Studerende”, den skal indeholde tre properties. Disse tre properties skal være synlige uden for klassen men må kun sættes indenfor denne klasse.
    
    string Navn
    int fødselsmåned
    int fødselsdag

---
+++ ClassRoom/Studerende.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class Studerende
+    {
+
+        private string _navn;
+        private int _fødselsdag;
+        private int _fødselsmåned;
+
+
+        public string navn { get; }
+        public int fødselsdag { get; }
+        public int fødselsmåned { get; }
+
+
+        public Studerende(string navn, int fødselsdag, int fødselsmåned)
+        {
+            _navn = navn;
+            _fødselsdag = fødselsdag;
+            _fødselsmåned = fødselsmåned;
+        }
+    }
+}
